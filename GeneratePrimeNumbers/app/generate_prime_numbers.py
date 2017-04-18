5f4923fa4eae57de28a0580df796471e327126d3
Male Michael <malmike21@outlook.com>
2017-04-18 11:17:25 +0300

    Added condition to check that value inserted is a number

--- GeneratePrimeNumbers/app/generate_prime_numbers.py
+++ GeneratePrimeNumbers/app/generate_prime_numbers.py
@@ -1,2 +1,6 @@
-def generatePrimeNumbers(value):
-    pass
+def generatePrimeNumbers(num):
+    numbers_types = (int, float, complex)
+    if isinstance(num, numbers_types):
+        pass
+    else:
+        raise TypeError
