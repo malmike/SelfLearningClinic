68148a57afb86d4094627c8f0a93e437fa274f2c
Male Michael <malmike21@outlook.com>
2017-04-18 12:20:08 +0300

    Added initial prime number 2 prime list. Added variable nextPrime to hold the expected next prime number

--- GeneratePrimeNumbers/app/generate_prime_numbers.py
+++ GeneratePrimeNumbers/app/generate_prime_numbers.py
@@ -1,4 +1,4 @@
-import math
+from math import sqrt
 
 def generatePrimeNumbers(num):
     numbers_types = (int, float, complex)
@@ -6,7 +6,13 @@ def generatePrimeNumbers(num):
     if isinstance(num, numbers_types):
         #Check that the value inserted is greater than or equal to 3
         if num >= 3:
+            #List to hold the prime numbers
             prime_list = []
+            #Added initial prime number 2
+            prime_list.append(2)
+            nextPrime = 3
+        
+            #Return prime_list        
             return prime_list
         else:
             raise ValueError
