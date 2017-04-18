510afb08c60935466b78e2d7ed7074a0241d15d1
Male Michael <malmike21@outlook.com>
2017-04-18 11:19:15 +0300

    Put condition to check that the value inserted is greater than 3 else return value error

--- GeneratePrimeNumbers/app/generate_prime_numbers.py
+++ GeneratePrimeNumbers/app/generate_prime_numbers.py
@@ -1,6 +1,13 @@
+import math
+
 def generatePrimeNumbers(num):
     numbers_types = (int, float, complex)
+    #Check that the value inserted is a number
     if isinstance(num, numbers_types):
-        pass
+        #Check that the value inserted is greater than or equal to 3
+        if num >= 3:
+            pass
+        else:
+            raise ValueError
     else:
         raise TypeError
