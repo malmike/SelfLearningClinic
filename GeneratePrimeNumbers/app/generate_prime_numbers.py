75fb97847e7c2729f0ed3ccf12be37d4c108f29e
Male Michael <malmike21@outlook.com>
2017-04-18 12:23:20 +0300

    Looped though values assigned to nextPrime that are below the argument value provided and checked if they are divisible by the values in prime_list that are less than the square root of the num. If not the value is a prime number and added to prime_list

--- GeneratePrimeNumbers/app/generate_prime_numbers.py
+++ GeneratePrimeNumbers/app/generate_prime_numbers.py
@@ -11,7 +11,25 @@ def generatePrimeNumbers(num):
             #Added initial prime number 2
             prime_list.append(2)
             nextPrime = 3
-        
+            #Set loop to get prime numbers less than num
+            while nextPrime < num:
+                #Set bool isPrime to True
+                isPrime = True
+                #Get the square root of nextPrime
+                sqrt_value = sqrt(nextPrime)
+                #Select values in prime_list that are less than sqrt_value and put them in a list sample_range
+                sample_range = [i for i in prime_list if i <= sqrt_value]
+                #Loop through the sample_range values
+                for i in sample_range:
+                    #Test that the value nextPrime is not divisible by the values in sample_range
+                    if nextPrime%i == 0:
+                        #If the value is divisible set isPrime to false and break out of the loop
+                        isPrime = False
+                        break
+                #If isPrime is still set to true then add value to prime_list
+                if isPrime:
+                    prime_list.append(nextPrime)
+                nextPrime += 2
             #Return prime_list        
             return prime_list
         else:
