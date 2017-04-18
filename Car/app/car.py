c265a0cbf07217d1c690a80619a24b374a779094
Male Michael <malmike21@outlook.com>
2017-04-18 23:12:06 +0300

    Applied fixes to the car class

--- Car/app/car.py
+++ Car/app/car.py
@@ -1,36 +1,38 @@
 class Car(object):
     def __init__(self, *args, **kwargs):
         self.type = ''
-        if len(args)>=1:
-            self.type = args[0]
+        if len(args)==3:
+            self.type = args[2]
         if len(args)>=2:
             self.model = args[1]
         else:
             self.model = 'GM'
-        if len(args)==3:
-            self.name = args[2]
+        if len(args)>=1:
+            self.name = args[0]
         else:
             self.name = 'General'
         self.speed = 0
-        if self.type != '':
-            self.assign_doors()
-            self.assign_wheels()												
+        self.assign_doors()
+        self.assign_wheels()
+
     def assign_doors(self):
-        if self.type == 'Koenigsegg' or self.type == 'Porshe':
+        if self.name == 'Koenigsegg' or self.name == 'Porshe':
             self.num_of_doors = 2
         else:
             self.num_of_doors = 4
     def assign_wheels(self):
-        if self.type == 'MAN':
+        if self.type == 'trailer':
             self.num_of_wheels = 8
         else:
             self.num_of_wheels = 4
     def is_saloon(self):
-        if self.type != 'MAN':
+        if self.type != 'trailer':
             return True
-    def drive(self, value):
-        if self.type == 'MAN':
-            speed = value*11
         else:
-            speed = value*(1000/3)
-            return Car()
+            return False
+    def drive(self, value):
+        if self.type == 'trailer':
+            self.speed = 77
+        else:
+            self.speed = 10**value
+        return self
