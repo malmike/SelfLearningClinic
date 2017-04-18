5c469d448896fa89452efaa7f0eabec49b48aa66
Male Michael <malmike21@outlook.com>
2017-04-18 10:37:13 +0300

    Re-ordered the tests basing on the

--- GeneratePrimeNumbers/test/test_generate_prime_numbers.py
+++ GeneratePrimeNumbers/test/test_generate_prime_numbers.py
@@ -1,4 +1,5 @@
 import unittest
+from math import sqrt
 
 class TestGeneratePrimeNumbers(unittest.TestCase):
     
@@ -10,13 +11,13 @@ class TestGeneratePrimeNumbers(unittest.TestCase):
         self.assertRaises(TypeError, generatePrimeNumbers, 'two')
 
     def test_generate_prime_numbers_returns_error_if_arg_is_less_than_three(self):
-        self.assertRaises(ValueError, generatePrimeNumbers, 'two')
+        self.assertRaises(ValueError, generatePrimeNumbers, 2)
+
+    def test_generate_prime_numbers_returns_list(self):
+        self.assertTrue(isinstance(generatePrimeNumbers(self.arg_limit), list))
     
     def test_list_returned_contains_values_greater_than_one(self):
         self.assertTrue(all(x >= 2 for x in generatePrimeNumbers(self.arg_limit)))
-    
-    def test_generate_prime_numbers_returns_list(self):
-        self.assertTrue(isinstance(generatePrimeNumbers(self.arg_limit), list))
 
     def test_generate_prime_numbers_returns_prime_numbers_less_than_arg(self):
         self.assertTrue(all(x < self.arg_limit for x in generatePrimeNumbers(self.arg_limit)))
@@ -24,7 +25,8 @@ class TestGeneratePrimeNumbers(unittest.TestCase):
     def test_generate_prime_numbers_returns_only_prime_numbers(self):
         isPrime = True
         for x in generatePrimeNumbers(self.arg_limit):
-            sample_range = [i for i in self.sample_prime if i < x]
+            sqrt_value = sqrt(x)
+            sample_range = [i for i in self.sample_prime if i < sqrt_value]
             for y in sample_range:
                 if x%y == 0:
                     isPrime = False
