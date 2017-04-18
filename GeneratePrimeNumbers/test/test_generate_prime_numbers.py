be3dc30a9d165505084dcfbaaaab4f4438d71158
Male Michael <malmike21@outlook.com>
2017-04-18 12:19:57 +0300

    Added test to check that all prime numbers are generated that are below the argument provided

--- GeneratePrimeNumbers/test/test_generate_prime_numbers.py
+++ GeneratePrimeNumbers/test/test_generate_prime_numbers.py
@@ -6,13 +6,17 @@ class TestGeneratePrimeNumbers(unittest.TestCase):
     
     def setUp(self):
         self.arg_limit = 32
-        self.sample_prime = [2,3,5,7,11,13,17,19,23]
+        self.sample_prime = [2,3,5,7,911,13,17,19,23,29,31]
+        self.sample_prime_length = 11
 
     def test_generate_prime_numbers_returns_error_message_if_arg_not_number(self):
         self.assertRaises(TypeError, generatePrimeNumbers, 'two')
 
     def test_generate_prime_numbers_returns_error_if_arg_is_less_than_three(self):
         self.assertRaises(ValueError, generatePrimeNumbers, 2)
+
+    def test_generate_prime_numbers_returns_all_prime_numbers_less_than_arg(self):
+        self.assertEqual(self.sample_prime_length, len(generatePrimeNumbers(self.arg_limit)))
 
     def test_generate_prime_numbers_returns_list(self):
         self.assertTrue(isinstance(generatePrimeNumbers(self.arg_limit), list))
@@ -30,7 +34,7 @@ class TestGeneratePrimeNumbers(unittest.TestCase):
         isPrime = True
         for x in generatePrimeNumbers(self.arg_limit):
             sqrt_value = sqrt(x)
-            sample_range = [i for i in self.sample_prime if i < sqrt_value]
+            sample_range = [i for i in self.sample_prime if i <= sqrt_value]
             for y in sample_range:
                 if x%y == 0:
                     isPrime = False
