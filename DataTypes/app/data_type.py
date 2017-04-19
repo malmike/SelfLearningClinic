604c515789d23db333c3f58dd1b11908689f03b7
Male Michael <malmike21@outlook.com>
2017-04-19 07:14:22 +0300

    Added comments to data_types function

--- DataTypes/app/data_type.py
+++ DataTypes/app/data_type.py
@@ -1,12 +1,16 @@
 def data_type(arg):
-    
+    #Check that the argument passed is a list
     if isinstance(arg, list):
+        #If the list has more than 2 values return the 3rd value 
+        #Else return None
         if len(arg) > 2:
             return arg[2]
         else:
             return None
+    #Check that the argument is a bool, if so return its value
     elif isinstance(arg, bool):
         return arg
+    #Check that the argument is a number
     elif isinstance(arg, int):
         if arg < 100:
             return 'less than 100'
@@ -14,7 +18,9 @@ def data_type(arg):
             return 'equal than 100'
         else:
             return 'more than 100'
+    #Check that the argument is a string, if so return its length
     elif isinstance(arg, str):
         return len(arg)
+    #Check that no argument is passed
     elif arg is None:
         return 'no value'
