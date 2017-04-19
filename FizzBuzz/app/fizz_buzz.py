0893988d80b9af8ed9abe7e36a169114803d2335
Male Michael <malmike21@outlook.com>
2017-04-19 07:09:33 +0300

    Added comments to fizz buzz function

--- FizzBuzz/app/fizz_buzz.py
+++ FizzBuzz/app/fizz_buzz.py
@@ -1,14 +1,20 @@
 def fizz_buzz(arg):
     numbers_types = (int, float, complex)
+    #Check that the argument passed is a number
     if isinstance(arg, numbers_types):
+        #Initialise a variable value that will hold Fizz &/or Buzz
+        #Initialise a boolean used to show that Fizz &/or Buzz have been set
         value = ''
         divisible = False
+        #Check whether the argument is divisible by 3, if so add Fizz to value and set divisible to True
         if arg % 3 == 0:
             divisible = True
             value += 'Fizz'
+        #Check whether the argument is divisible by 5, if so add Buzz to value and set divisible to True
         if arg % 5 == 0:
             divisible = True
             value += 'Buzz'
+        #Check whether divisible is true, if so return value else return the argument passed
         if divisible:
             return value
         else:
